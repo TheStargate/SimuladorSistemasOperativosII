76235234b413ec43fa21964ac6619b93155f5daa
AaronTudor <Aaron.dn.tudor@gmail.com>
2025-02-15 20:13:56 +0100

    Update mi_mkfs.c
    Me da palo seguir. Seguiré mañana xd

--- nivel1/mi_mkfs.c
+++ nivel1/mi_mkfs.c
@@ -1,9 +1,18 @@
 
 #include "bloques.h"
 
+int main(int argc, char **argv)
+{
 
+    int nbloques;
 
-int main (int argcm, char **argv) {
-
-
+    bmount(argv[1]);
+    nbloques = atoi(argv[2]); // Usamos función atoi para convertir string de consola a entero
+    unsigned char array[BLOCKSIZE];
+    for (int i = 0; i < nbloques; i++)
+    {
+        memset(array, 0, BLOCKSIZE);
+        bwrite(i, array);
+    }
+    bumount();
 }
