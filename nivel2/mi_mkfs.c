9d651684507af2ce30849a6286421912663147b2
Miguel Sansó Febrer <miguel.sanso3@estudiant.uib.cat>
2025-02-18 19:00:09 +0100

    Update del mi_mkfs.c el nivel 2

--- nivel2/mi_mkfs.c
+++ nivel2/mi_mkfs.c
@@ -1,17 +1,8 @@
-#include "bloques.h"
+#include "ficheros_basico.h"
 
 int main(int argc, char **argv)
 {
 
-    int nbloques;
+    
 
-    bmount(argv[1]);
-    nbloques = atoi(argv[2]); // Usamos función atoi para convertir string de consola a entero
-    unsigned char array[BLOCKSIZE];
-    for (int i = 0; i < nbloques; i++)
-    {
-        memset(array, 0, BLOCKSIZE);
-        bwrite(i, array);
-    }
-    bumount();
 }
