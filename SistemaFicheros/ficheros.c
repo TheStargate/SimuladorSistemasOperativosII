75ed333a27e5d6887d8b2395705b51fb03cc5af7
Miguel Sansó Febrer <miguel.sanso3@estudiant.uib.cat>
2025-03-13 13:06:55 +0100

    classe nivel 5

--- SistemaFicheros/ficheros.c
+++ SistemaFicheros/ficheros.c
@@ -18,19 +18,43 @@
 int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes)
 {
     struct inodo inodo;
-    
+    unsigned char buf_bloque[BLOCKSIZE];
+
     if ((inodo.permisos & 2) != 2)
     {
         fprintf(stderr, RED "No hay permisos de escritura\n" RESET);
         return FALLO;
     }
-    int primerBL = offset/BLOCKSIZE;
-    int ultimoBL = (offset +nbytes-1) / BLOCKSIZE;
 
-    //Desplazamiento donde cae el offset y los nbytes escritos a partir del offset
+    // Primer y último bloque lógico
+    int primerBL = offset / BLOCKSIZE;
+    int ultimoBL = (offset + nbytes - 1) / BLOCKSIZE;
+
+    // Desplazamiento donde cae el offset y los nbytes escritos a partir del offset
     int desp1 = offset % BLOCKSIZE;
-    int desp2 = (offset + nbytes -1 ) % BLOCKSIZE;
+    int desp2 = (offset + nbytes - 1) % BLOCKSIZE;
 
+    if (primerBL == ultimoBL)
+    {
+
+        int nbfisico = traducir_bloque_inodo(ninodo, primerBL, 1);
+        if (bread(nbfisico, buf_bloque) == FALLO)
+            return FALLO;
+        memcpy(buf_bloque + desp1, buf_original, nbytes);
+        if (bwrite(nbfisico, buf_bloque) == FALLO)
+            return FALLO;
+    }
+    else
+    {
+        int nbfisico = traducir_bloque_inodo(ninodo, primerBL, 1);
+        if(bread(nbfisico, buf_bloque) == FALLO);
+            return FALLO;
+        memcpy(buf_bloque + desp1, buf_original, nbytes);
+        if (bwrite(nbfisico, buf_bloque) == FALLO)
+            return FALLO;
+
+
+    }
 
     return 7;
 }
@@ -47,7 +71,52 @@ int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offse
  */
 int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsigned int nbytes)
 {
-    return 7;
+    struct inodo inodo;
+
+    // Primer y último bloque lógico
+    int primerBL = offset / BLOCKSIZE;
+    int ultimoBL = (offset + nbytes - 1) / BLOCKSIZE;
+
+    // Buffer para almacenar el bloque
+    unsigned char buf_bloque[BLOCKSIZE];
+
+    // Desplazamiento donde cae el offset y los nbytes escritos a partir del offset
+    int desp1 = offset % BLOCKSIZE;
+    int desp2 = (offset + nbytes - 1) % BLOCKSIZE;
+
+    // Revisar permisos
+    if ((inodo.permisos & 4) != 4)
+    {
+        fprintf(stderr, RED "No hay permisos de lectura\n" RESET);
+        return FALLO;
+    }
+
+    // Comprobar si el offset es mayor o igual que el tamaño tamEnBytesLog
+    if (offset >= inodo.tamEnBytesLog)
+    {
+        return 0;
+    }
+    else if ((offset + nbytes) >= inodo.tamEnBytesLog)
+    {
+        return inodo.tamEnBytesLog - offset;
+    }
+
+    // Contemplar el caso en que el fichero/directorio ocupe un único bloque lógico
+    if (primerBL == ultimoBL)
+    {
+
+        unsigned char buf_bloque[BLOCKSIZE];
+
+        int nbfisico = traducir_bloque_inodo(ninodo, primerBL, 1);
+        if (bread(nbfisico, buf_bloque) == FALLO)
+            return FALLO;
+        memcpy(buf_bloque + desp1, buf_original, nbytes);
+        if (bwrite(nbfisico, buf_bloque) == FALLO)
+            return FALLO;
+    }
+    else
+    {
+    }
 }
 
 /**
@@ -60,12 +129,15 @@ int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsi
  */
 int mi_stat_f(unsigned int ninodo, struct STAT *p_stat)
 {
-    mi_read_f(ninodo, );
-
     struct inodo inodo;
 
-    leer_inodo(ninodo, &inodo);
+    // Leemos el inodo
+    if (leer_inodo(ninodo, &inodo) == FALLO)
+    {
+        return FALLO;
+    }
 
+    // Guardamos los datos del inodo en p_stat
     p_stat->atime = inodo.atime;
     p_stat->btime = inodo.btime;
     p_stat->ctime = inodo.ctime;
@@ -75,39 +147,39 @@ int mi_stat_f(unsigned int ninodo, struct STAT *p_stat)
     p_stat->permisos = inodo.permisos;
     p_stat->tamEnBytesLog = inodo.tamEnBytesLog;
     p_stat->tipo = inodo.tipo;
-    
+
     return EXITO;
 }
 
 /**
- * Cambia los permisos de un fichero/directorio (correspondiente al nº de inodo pasado como argumento, ninodo) con el valor que indique el argumento permisos. * 
+ * Cambia los permisos de un fichero/directorio (correspondiente al nº de inodo pasado como argumento, ninodo) con el valor que indique el argumento permisos. *
  * con el valor que indique el argumento permisos.
  *
  * @param ninodo Número de inodo que queremos leer
  * @param permisos Permisos que queremos asignar al fichero/directorio
  * @return EXITO si todo ha ido bien, FALLO si ha habido algún error.
-*/
+ */
 int mi_chmod_f(unsigned int ninodo, unsigned char permisos)
 {
     struct inodo inodo;
-    
+
     // Leer el inodo
-    if (leer_inodo(ninodo, &inodo) == FALLO) {
-        fprintf(stderr, "Error en mi_chmod_f: No se pudo leer el inodo %u\n", ninodo);
+    if (leer_inodo(ninodo, &inodo) == FALLO)
+    {
         return FALLO;
     }
-    
+
     // Actualizar los permisos
     inodo.permisos = permisos;
-    
-    // Actualizar ctime (tiempo de cambio)
+
+    // Actualizar ctime
     inodo.ctime = time(NULL);
-    
+
     // Escribir el inodo modificado
-    if (escribir_inodo(ninodo, &inodo) == FALLO) {
-        fprintf(stderr, "Error en mi_chmod_f: No se pudo escribir el inodo %u\n", ninodo);
+    if (escribir_inodo(ninodo, &inodo) == FALLO)
+    {
         return FALLO;
     }
-    
+
     return EXITO;
 }
