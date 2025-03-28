3604d5f3eb48ab3d0df908eb194eacd8330e5355
Miguel Sansó Febrer <miguel.sanso3@estudiant.uib.cat>
2025-03-28 15:23:42 +0100

    truncar no se si esta bien

---
+++ SistemaFicheros/truncar.c
@@ -0,0 +1,34 @@
+/*
+ * Autores: [Aaron Satyar Daghigh-Nia Tudor, Jaume Juan Huguet, Miguel Sansó Febrer]
+ * Descripción: Programa para truncar un fichero/directorio a un tamaño específico
+ */
+
+#include "ficheros.h"
+
+int main(int argc, char **argv)
+{
+    // Comprobar si el número de argumentos es correcto y si no mostrar sintaxis
+    if (argc != 4)
+    {
+        fprintf(stderr, RED "Número de argumentos incorrecto. Sintaxis: ./permitir <nombre_dispositivo> <ninodo> <permisos>\n" RESET);
+        return FALLO;
+    }
+
+    int ninodo = atoi(argv[2]);
+    int nbytes = atoi(argv[3]);
+
+    bmount(argv[1]);
+
+    if (nbytes < 0)
+    {
+        liberar_inodo(ninodo);
+    }
+    else
+    {
+        mi_truncar_f(ninodo, nbytes);
+    }
+
+    bumount();
+
+    return EXITO;
+}
