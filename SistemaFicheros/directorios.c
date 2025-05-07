6d8cccefc4f333294c70bf23a89a61f2be5e6f83
TheStargate <holahatche@gmail.com>
2025-05-07 16:21:39 +0200

    cambios tontos luego sigo

--- SistemaFicheros/directorios.c
+++ SistemaFicheros/directorios.c
@@ -372,7 +372,8 @@ int mi_dir(const char *camino, char *buffer, char tipo, char flag)
         else
             strcat(buffer, "-");
         strcat(buffer, "|");
-        struct tm *tm; // ver info: struct tm
+        
+        struct tm *tm;
         tm = localtime(&inodo.mtime);
         sprintf(tmp, "%d-%02d-%02d %02d:%02d:%02d", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
         strcat(buffer, tmp);
